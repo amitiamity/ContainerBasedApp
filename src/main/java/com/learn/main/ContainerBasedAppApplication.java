e2a019af19968e93e481c1e5039e511fe885bbc8
Amit Patel <amitptl.in@gmail.com>
2018-11-09 10:50:24 +0530

    initial commit

---
+++ src/main/java/com/learn/main/ContainerBasedAppApplication.java
@@ -0,0 +1,12 @@
+package com.learn.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerBasedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerBasedAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/learn/main/ContainerBasedAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learn.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContainerBasedAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
